2e6cf8ca90c7d71ad268e7622b896a066365c510
Matheus Schumann <158315408+MatheusSchummann@users.noreply.github.com>
2024-08-27 15:50:39 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace aula4_cs_ex1
 {
+    
+    /*Escreva um programa em C#, seguindo as orientações do início deste documento. O
+    seu programa deve receber 3 valores v1, v2 e v3 (inteiros), e retorná-los em
+    ordem crescente*/
+    
     public class Program
     {
         static void Main(string[] args)
@@ -27,7 +32,7 @@ namespace aula4_cs_ex1
             listaA = Converter.conversor(listaS);
             Converter.logica(listaA);
 
-            Console.WriteLine("Os valores após o sort ficaram:");
+            Console.WriteLine("Os valores após o sort foram:");
             for (int i = 0; i < 3; i++)         
             {
                 Console.WriteLine("{0}",listaA[i]);
