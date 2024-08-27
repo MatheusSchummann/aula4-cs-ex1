03185d138558c99d8d10785d62b31cffff2ed0d0
suporte3 <suporte3@DESK-SD0149>
2024-08-27 09:51:04 -0300

    Adicionar arquivos de projeto.

---
+++ Convert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula4_cs_ex1
+{
+    public class Convert
+    {
+        public List<int> conversor(List<string> listaS)
+        {
+            List<int> listaA = new List<int>();
+            listaA = listaS.ConvertAll(int.Parse);
+            return listaA;
+        }
+        public List<int> logica(List<int> listaA)
+        {
+            listaA.Sort();
+            return listaA;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula4_cs_ex1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program(); 
+        }
+        public Program()
+        {
+            Convert Converter = new Convert();
+
+            List<string> listaS = new List<string>();
+            List<int> listaA = new List<int>();
+
+            for(int i = 0; i < 3; i++) 
+            {
+                Console.Write("Digite o valor {0}/3: ", i+1);
+                listaS.Add(Console.ReadLine());
+            }
+            listaA = Converter.conversor(listaS);
+            Converter.logica(listaA);
+
+            Console.WriteLine("Os valores após o sort ficaram:");
+            for (int i = 0; i < 3; i++)         
+            {
+                Console.WriteLine("{0}",listaA[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
